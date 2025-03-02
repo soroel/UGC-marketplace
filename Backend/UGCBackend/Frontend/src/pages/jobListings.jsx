0552284d5f4d1a9442c53ededc26ce0d163813c1
soroel <sorophinenoel@gmail.com>
2025-03-02 14:41:33 +0300

    recover from conflicts

--- Backend/UGCBackend/Frontend/src/pages/jobListings.jsx
+++ Backend/UGCBackend/Frontend/src/pages/jobListings.jsx
@@ -3,12 +3,11 @@ import { Card, CardContent } from "../components/ui/card";
 import { Button } from "../components/ui/button";
 import { Search, Briefcase } from "lucide-react";
 import Navbar from "../components/Navigation";
+import SMSPopup from "../components/SMSPopup";
 
 export default function Jobs() {
   const [search, setSearch] = useState("");
   const [showPopup, setShowPopup] = useState(false);
-<<<<<<< HEAD
-<<<<<<< HEAD
 
   // Sample data for jobs and brands
   const [jobs] = useState([
@@ -32,10 +31,6 @@ export default function Jobs() {
   const filteredBrands = brands.filter((brand) =>
     brand.name.toLowerCase().includes(search.toLowerCase())
   );
-=======
->>>>>>> fa3cd0cd77533018a69cdae829c33b506c65fb35
-=======
->>>>>>> fa3cd0cd77533018a69cdae829c33b506c65fb35
 
   useEffect(() => {
     if (!localStorage.getItem("hasSeenPopup")) {
@@ -101,15 +96,9 @@ export default function Jobs() {
           ))}
         </div>
       </section>
-<<<<<<< HEAD
-=======
 
       {/* SMS Popup Component */}
       <SMSPopup showPopup={showPopup} setShowPopup={setShowPopup} />
-<<<<<<< HEAD
->>>>>>> fa3cd0cd77533018a69cdae829c33b506c65fb35
-=======
->>>>>>> fa3cd0cd77533018a69cdae829c33b506c65fb35
     </div>
   );
 }
--- Backend/UGCBackend/UGCBackend/urls.py
+++ Backend/UGCBackend/UGCBackend/urls.py
@@ -1,23 +1,8 @@
 from django.contrib import admin
-<<<<<<< HEAD
-<<<<<<< HEAD
-from django.urls import path,include
-from django.views.generic import TemplateView
-from django.conf import settings 
-from django.conf.urls.static import static  
-
-=======
 from django.urls import include, path
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
->>>>>>> fa3cd0cd77533018a69cdae829c33b506c65fb35
-=======
-from django.urls import include, path
-from django.views.generic import TemplateView
-from django.conf import settings
-from django.conf.urls.static import static
->>>>>>> fa3cd0cd77533018a69cdae829c33b506c65fb35
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -27,12 +12,5 @@ urlpatterns = [
 ]
 
 # Serve media files during development
-<<<<<<< HEAD
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-=======
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-<<<<<<< HEAD
->>>>>>> fa3cd0cd77533018a69cdae829c33b506c65fb35
-=======
->>>>>>> fa3cd0cd77533018a69cdae829c33b506c65fb35
